c2094ec0b0426334c373b2c885f94bbc4e642f87
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-10-01 10:43:40 +0000

    Palette generation using input added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const searchInput = document.querySelector(".search-input"),
+const searchInput = document.querySelector("#search-input"),
 	searchColor = document.querySelector(".search-color"),
 	searchImage = document.querySelector("#search-image"),
 	typeSelect = document.querySelector("#palette-type"),
 	countSelect = document.querySelector("#palette-count"),
 	randomBtn = document.querySelector("#random-btn"),
 	paletteContainer = document.querySelector("#palette"),
-	relatedConatiner = document.querySelector("#related");
+	relatedContainer = document.querySelector("#related");
 
 let currentColor = "skyblue",
 	currentType = "analogous",
@@ -148,3 +148,126 @@ function generatePalette(hsl, type, count) {
 			return generateRelatedPalette(hsl, count);
 	}
 }
+
+function generatePaletteHTML(type, container) {
+	let color = currentColor;
+	let count = currentCount;
+	let hsl = getHslFromcolor(color);
+	if (!hsl) {
+		return;
+	}
+	let palette = [];
+	container.innerHTML = "";
+	palette = generatePalette(hsl, type, count);
+	palette.forEach((color) => {
+		color = HslToHex(color);
+		const colorEl = document.createElement("div");
+		colorEl.classList.add("color");
+		colorEl.style.backgroundColor = color;
+		colorEl.innerHTML = `
+			<div class="overlay">
+				<div class="icons">
+					<div class="copy-color">
+						<i class="far fa-copy"></i>
+					</div>
+					<div class="generate-palette">
+						<i class="fas fa-palette"></i>
+					</div>
+				</div>
+				<div class="code">
+					${color}
+				</div>
+			</div>
+		`;
+		container.appendChild(colorEl);
+	});
+}
+
+function getHslFromcolor(color) {
+	let hsl;
+	if (isValidColor(color)) {
+		let temp = document.createElement("div");
+		temp.style.color = color;
+		document.body.appendChild(temp);
+		let style = window.getComputedStyle(temp, null);
+		let rgb = style.getPropertyValue("color");
+		document.body.removeChild(temp);
+		rgb = removeRGB(rgb);
+		hsl = rbgToHsl(rgb);
+		return hsl;
+	}
+}
+
+function isValidColor(color) {
+	return CSS.supports("color", color);
+}
+
+function removeRGB(rgb) {
+	return rgb.replace("rgb(", "").replace(")", "").split(",");
+}
+
+function rbgToHsl(rgb) {
+	let r = rgb[0] / 255;
+	let g = rgb[1] / 255;
+	let b = rgb[2] / 255;
+
+	let cmin = Math.min(r, g, b);
+	let cmax = Math.max(r, g, b);
+	let delta = cmax - cmin;
+	let h = 0;
+	let s = 0;
+	let l = (cmax + cmin) / 2;
+
+	if (delta == 0) {
+		h = 0;
+		s = 0;
+	} else {
+		if (cmax == r) {
+			h = ((g - b) / delta) % 6;
+		} else if (cmax == g) {
+			h = (b - r) / delta + 2;
+		} else {
+			h = (r - g) / delta + 4;
+		}
+		h = Math.round(h * 60);
+		if (h < 0) {
+			h += 360;
+		}
+		s = delta != 0 ? delta / (1 - Math.abs(2 * l - 1)) : 0;
+	}
+
+	s = Math.round(s * 100);
+	l = Math.round(l * 100);
+
+	return [h, s, l];
+}
+
+function HslToHex(hsl) {
+	let h = hsl[0];
+	let s = hsl[1];
+	let l = hsl[2];
+	l /= 100;
+	const a = (s * Math.min(l, 1 - l)) / 100;
+	const f = (n) => {
+		const k = (n + h / 30) % 12;
+		const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+		return Math.round(255 * color)
+			.toString(16)
+			.padStart(2, "0");
+	};
+	return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+generatePaletteHTML(currentType, paletteContainer);
+generatePaletteHTML("related", relatedContainer);
+
+searchInput.addEventListener("keyup", (e) => {
+	const value = e.target.value;
+	if (isValidColor(value)) {
+		searchColor.style.backgroundColor = value;
+		currentColor = value;
+		generatePaletteHTML(currentType, paletteContainer);
+		generatePaletteHTML("related", relatedContainer);
+	}
+});
+
