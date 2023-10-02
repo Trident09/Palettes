21d34c732a4cd471e27d0796d52fb854ecffd144
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-10-02 09:19:04 +0000

    added all download functionality

--- script.js
+++ script.js
@@ -377,3 +377,108 @@ function extractColorsFromImage(image) {
 		imageColorsWrapper.classList.remove("hidden");
 	});
 }
+
+const downloadBtn = document.querySelector("#download-btn"),
+	downloadFormat = document.querySelector("#download-format"),
+	downloadName = document.querySelector("#download-name");
+
+downloadBtn.addEventListener("click", () => {
+	const format = downloadFormat.value;
+	let name = downloadName.value;
+	name = name == "" ? "Palette" : name;
+	downloadPalette(format, name);
+});
+
+function downloadPalette(format, name) {
+	const palette = document.querySelector("#palette");
+	const paletteColors = palette.querySelectorAll(".color");
+	const colors = [];
+	paletteColors.forEach((color) => {
+		colors.push(color.style.backgroundColor);
+	});
+	switch (format) {
+		case "png":
+			downloadPaletteAsPng(colors, name);
+			break;
+		case "svg":
+			downloadPaletteAsSvg(colors, name);
+			break;
+		case "css":
+			downloadPaletteAsCss(colors, name);
+			break;
+		case "json":
+			downloadPaletteAsJson(colors, name);
+			break;
+		default:
+			break;
+	}
+}
+
+function downloadPaletteAsPng(colors, name) {
+	const canvas = document.createElement("canvas");
+	canvas.width = colors.length * 200;
+	canvas.height = 1000;
+	const ctx = canvas.getContext("2d");
+	colors.forEach((color, index) => {
+		ctx.fillStyle = color;
+		ctx.fillRect(index * 200, 0, 200, 1000);
+	});
+	const link = document.createElement("a");
+	link.download = name + ".png";
+	link.href = canvas.toDataURL();
+	link.click();
+}
+
+function downloadPaletteAsSvg(colors, name) {
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+	svg.setAttribute("width", "100%");
+	svg.setAttribute("height", "100%");
+	svg.setAttribute("viewbox", "0 0 100 100");
+	svg.setAttribute("preserveAspectRatio", "none");
+	colors.forEach((color, index) => {
+		const rect = document.createElementNS(
+			"http://www.w3.org/2000/svg",
+			"rect"
+		);
+		const width = 100 / colors.length;
+		rect.setAttribute("x", index * width);
+		rect.setAttribute("y", 0);
+		rect.setAttribute("width", width);
+		rect.setAttribute("height", 100);
+		rect.setAttribute("fill", color);
+		svg.appendChild(rect);
+	});
+
+	const svgData = new XMLSerializer().serializeToString(svg);
+	const svgBlob = new Blob([svgData], {
+		type: "image/svg+xml;charset=utf-8",
+	});
+	const svgUrl = URL.createObjectURL(svgBlob);
+	const downloadLink = document.createElement("a");
+	downloadLink.download = name + ".svg";
+	downloadLink.href = svgUrl;
+	downloadLink.click();
+}
+
+function downloadPaletteAsCss(colors, name) {
+	const css = `:root {
+		${colors.map((color, index) => `--color-${index + 1}: ${color};`).join("\n")}
+	}`;
+	const blob = new Blob([css], { type: "text/css" });
+	const url = URL.createObjectURL(blob);
+	const link = document.createElement("a");
+	link.download = name + ".css";
+	link.href = url;
+	link.click();
+}
+
+function downloadPaletteAsJson(colors, name) {
+	const json = JSON.stringify(colors);
+	const blob = new Blob([json], { type: "application/json" });
+	const url = URL.createObjectURL(blob);
+	const link = document.createElement("a");
+	link.download = name + ".json";
+	link.href = url;
+	link.click();
+}
